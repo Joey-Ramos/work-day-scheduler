9321a832a0e2b97aa84235b3fee13cd8529c6455
Joey Ramos <jramos1202@gmail.com>
2020-06-22 02:08:58 -0500

    added save button box

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Add Current Date to Top of Webpage Variable
-var todaysDate = moment().format('dddd , MMMM Do');
+var todaysDate = moment().format('dddd, MMMM Do');
 
 // Current Day Function
 $("#currentDay").text(function() {
@@ -8,3 +8,5 @@ $("#currentDay").text(function() {
 
 
 // Time Blocks
+var currentHour = moment().format('h A');
+console.log(currentHour)
