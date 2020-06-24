dcc21d6c58571eb64c117dbadea553ef750e0f45
Joey Ramos <jramos1202@gmail.com>
2020-06-23 21:27:21 -0500

    added function for past present future

--- Develop/script.js
+++ Develop/script.js
@@ -8,5 +8,19 @@ $("#currentDay").text(function() {
 
 
 // Time Blocks
-var currentHour = moment().format('h A');
-console.log(currentHour)
+var currentHour = moment().format('h');
+console.log(currentHour);
+
+
+Array.from(document.querySelectorAll(".row")).forEach(function(element){
+    var time = element.getElementsByTagName('p')[0].textContent.split(/\D/ig)[0]
+    var textArea = element.getElementsByTagName('textarea')[0]
+    console.log(time)
+    if (currentHour < time) {
+        textArea.classList.add("past")
+    }
+    if (currentHour === time) 
+        textArea.classList.add("present")
+    if (currentHour > time)
+        textArea.classList.add("future")
+})
