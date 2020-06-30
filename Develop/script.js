e1f5961fb71f0d44666bbfdf7ec0feb1572d69ce
Joey Ramos <jramos1202@gmail.com>
2020-06-30 02:21:11 -0500

    added button element and styling around the span elements, changed time from 12hr to 24hr

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ $("#currentDay").text(function() {
 
 
 // Time Blocks
-var currentHour = moment().format('h');
+var currentHour = moment().format('HH');
 console.log(currentHour);
 
 
@@ -16,11 +16,18 @@ Array.from(document.querySelectorAll(".row")).forEach(function(element){
     var time = element.getElementsByTagName('p')[0].textContent.split(/\D/ig)[0]
     var textArea = element.getElementsByTagName('textarea')[0]
     console.log(time)
-    if (currentHour < time) {
+    if (currentHour > time) {
         textArea.classList.add("past")
     }
     if (currentHour === time) 
         textArea.classList.add("present")
-    if (currentHour > time)
+    if (currentHour < time)
         textArea.classList.add("future")
+})
+
+//var textContent = document.getElementsById('#text-content');
+//var addTask = document.getElementById("#add-task");
+
+$("#add-tasK").click(function() {
+    console.log("span was clicked")
 })
